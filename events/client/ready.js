88f9caec95b64927413b8cbe331bbf538a53064b
Stefan Schneider <55014443+SGSchneider@users.noreply.github.com>
2023-05-08 15:33:34 -0300

    Increased delay between stream status checks

--- events/client/ready.js
+++ events/client/ready.js
@@ -12,7 +12,7 @@ module.exports = {
     async execute(client){
         console.log(client.user.tag + " Está Logado")
         await LoadCommands(client)
-        setInterval(async() => {await CheckStream(client)}, 10000)
+        setInterval(async() => {await CheckStream(client)}, 300000)
     }
 
 }
@@ -21,21 +21,20 @@ async function CheckStream(client){
     let user = "s_schneider"
     const status = await fetch.get(`https://decapi.me/twitch/uptime/${user}`)
     if(status.body != "s_schneider is offline"){
-        const uptime = await fetch.get(`https://decapi.me/twitch/uptime/${user}`)
-        const title = await fetch.get(`https://decapi.me/twitch/title/${user}`)
-        const game = await fetch.get(`https://decapi.me/twitch/game/${user}`)
-        const logoCanal = await fetch.get(`https://decapi.me/twitch/avatar/${user}`)
-
-
-        const embed = new EmbedBuilder()
-        .setTitle("LIVE ON MEUS QUERIDOS")
-        .setDescription(`${title.body} \n Jogando ${game.body}`)
-        .setURL("https://twitch.tv/s_schneider")
-        .setImage(`${logoCanal.text}`)
-        if (flag!=0){
+        if (flag != 0){
             //client.channels.cache.get("549739489514487818").messages.fetch(mensagem.id).then((msg) => msg.edit({embeds : [embed]}))
         }
         else{
+            const title = await fetch.get(`https://decapi.me/twitch/title/${user}`)
+            const game = await fetch.get(`https://decapi.me/twitch/game/${user}`)
+            const logoCanal = await fetch.get(`https://decapi.me/twitch/avatar/${user}`)
+
+
+            const embed = new EmbedBuilder()
+            .setTitle("LIVE ON MEUS QUERIDOS")
+            .setDescription(`${title.body} \n Jogando ${game.body}`)
+            .setURL("https://twitch.tv/s_schneider")
+            .setImage(`${logoCanal.text}`)
             mensagem = await client.channels.cache.get("549739489514487818").send({embeds : [embed], content : "@everyone"})
             flag = 1
         } 
