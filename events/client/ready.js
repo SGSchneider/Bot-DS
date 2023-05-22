0a3a2a36343ab584010a2b7a71b950eab437d019
Stefan Schneider <55014443+SGSchneider@users.noreply.github.com>
2023-05-22 12:41:50 -0300

    Catched connect timeout in function CheckStream

--- events/client/ready.js
+++ events/client/ready.js
@@ -20,36 +20,41 @@ module.exports = {
 }
 
 async function CheckStream(client){
-    let user = "s_schneider"
-    const status = await fetch.get(`https://decapi.me/twitch/uptime/${user}`)
-    if(status.body != "s_schneider is offline"){
-        if (flag != 0){
-            //client.channels.cache.get("549739489514487818").messages.fetch(mensagem.id).then((msg) => msg.edit({embeds : [embed]}))
+    try{
+        let user = "s_schneider"
+        const status = await fetch.get(`https://decapi.me/twitch/uptime/${user}`)
+        if(status.body != "s_schneider is offline"){
+            if (flag != 0){
+                //client.channels.cache.get("549739489514487818").messages.fetch(mensagem.id).then((msg) => msg.edit({embeds : [embed]}))
+            }
+            else{
+                const title = await fetch.get(`https://decapi.me/twitch/title/${user}`)
+                const game = await fetch.get(`https://decapi.me/twitch/game/${user}`)
+                const logoCanal = await fetch.get(`https://decapi.me/twitch/avatar/${user}`)
+
+
+                const embed = new EmbedBuilder()
+                .setTitle("LIVE ON MEUS QUERIDOS")
+                .setDescription(`${title.body} \n Jogando ${game.body}`)
+                .setURL("https://twitch.tv/s_schneider")
+                .setImage(`${logoCanal.text}`)
+                mensagem = await client.channels.cache.get("549739489514487818").send({embeds : [embed], content : "@everyone"})
+                flag = 1
+            } 
         }
         else{
-            const title = await fetch.get(`https://decapi.me/twitch/title/${user}`)
-            const game = await fetch.get(`https://decapi.me/twitch/game/${user}`)
-            const logoCanal = await fetch.get(`https://decapi.me/twitch/avatar/${user}`)
-
-
-            const embed = new EmbedBuilder()
-            .setTitle("LIVE ON MEUS QUERIDOS")
-            .setDescription(`${title.body} \n Jogando ${game.body}`)
-            .setURL("https://twitch.tv/s_schneider")
-            .setImage(`${logoCanal.text}`)
-            mensagem = await client.channels.cache.get("549739489514487818").send({embeds : [embed], content : "@everyone"})
-            flag = 1
-        } 
-    }
-    else{
-        if(flag != 0){
-            const embed = new EmbedBuilder()
-            .setTitle("STREAM OFFLINE")
-            .setDescription(`Mas não fique triste, logo logo tem mais!`)
-            .setURL("https://twitch.tv/s_schneider")
-            client.channels.cache.get("549739489514487818").messages.fetch(mensagem.id).then((msg) => msg.edit({embeds : [embed], content : ""}))
-            flag = 0
+            if(flag != 0){
+                const embed = new EmbedBuilder()
+                .setTitle("STREAM OFFLINE")
+                .setDescription(`Mas não fique triste, logo logo tem mais!`)
+                .setURL("https://twitch.tv/s_schneider")
+                client.channels.cache.get("549739489514487818").messages.fetch(mensagem.id).then((msg) => msg.edit({embeds : [embed], content : ""}))
+                flag = 0
+            }
         }
+    }catch(UND_ERR_CONNECT_TIMEOUT){
+        console.log("Failed to Check Stream!")
+        
     }
 }
 
